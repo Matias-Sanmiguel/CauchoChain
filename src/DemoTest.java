dc3a08163bdbf83b35bd051327d292a0014f8746
Matias-Sanmiguel <masanmiguel@uade.edu.ar>
2025-11-06 15:40:59 -0300

    DAO

--- src/DemoTest.java
+++ src/DemoTest.java
@@ -20,25 +20,27 @@ public class DemoTest {
             miner.mine(bc);
 
             float juanBal = bc.getBalance(juan.getAddress());
-            System.out.println("Balance juan después del minado: " + juanBal);
+            System.out.println("Balance juan despues del minado: " + juanBal);
             if (Math.abs(juanBal - 100.0f) > 0.001f) throw new RuntimeException("juan balance esperado 100, obtenido=" + juanBal);
 
             Transaction tx = juan.createTransaction(pancho.getAddress(), 40.0f, bc);
             bc.createTransaction(tx);
 
-            System.out.println("Minando bloque con la transacción juan -> pancho...");
+            System.out.println("Minando bloque con la transaccion juan -> pancho...");
             miner.mine(bc);
 
             float panchoBal = bc.getBalance(pancho.getAddress());
             float juanBalAfter = bc.getBalance(juan.getAddress());
+            float minerBal = bc.getBalance(miner.getAddress());
 
             System.out.println("Balance pancho: " + panchoBal);
             System.out.println("Balance juan ahora: " + juanBalAfter);
+            System.out.println("Balance minero: " + minerBal);
 
             if (Math.abs(panchoBal - 40.0f) > 0.001f) throw new RuntimeException("pancho balance esperado 40, obtenido=" + panchoBal);
-            if (Math.abs(juanBalAfter - 60.0f) > 0.001f) throw new RuntimeException("juan balance esperado 60, obtenido=" + juanBalAfter);
+            if (Math.abs(juanBalAfter - 100.0f) > 0.001f) throw new RuntimeException("juan balance esperado 100 (60 - 40tx + 50 recompensa + 30 anterior), obtenido=" + juanBalAfter);
 
-            if (!bc.isChainValid()) throw new RuntimeException("La cadena no es válida");
+            if (!bc.isChainValid()) throw new RuntimeException("La cadena no es valida");
 
             System.out.println("TEST PASSED");
         } catch (Exception e) {
